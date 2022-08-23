06681189fceeac25682fe3b5fde469eafc2e1572
Andy Marton <awmarton@gmail.com>
2022-08-23 18:26:39 -0400

    Added dummy endpoints

--- ninetynine/ninetynine.py
+++ ninetynine/ninetynine.py
@@ -1,7 +1,29 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def show_gameboard():
     return "This will show the gameboard"
+
+@app.route("/start", methods=['POST'])
+def start():
+    """Start the game"""
+    content = request.get_json()
+    return 'FOOBAR2k', 204
+
+@app.route("/draw", methods=['POST'])
+def draw():
+    """Draw a card into players hand"""
+    pass
+
+@app.route("/play", methods=['POST'])
+def play():
+    """Play a card and claim a space"""
+    pass
+
+@app.route("/reset", methods=['POST'])
+def reset():
+    """Reset the game board and player hands"""
+    pass
