4aafecd337de4562c4eac48f5a4c6d72795072de
Andy Marton <awmarton@gmail.com>
2022-08-24 22:12:23 -0400

    Add GET method for playing a card

--- ninetynine/ninetynine.py
+++ ninetynine/ninetynine.py
@@ -1,11 +1,18 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from ninetynine.board import Board
+from ninetynine.deck import Deck
+from ninetynine.hand import Hand
 
 app = Flask(__name__)
 
+board = Board()
+deck = Deck()
+red_hand = Hand('red')
+green_hand = Hand('green')
 
 @app.route("/")
 def show_gameboard():
-    return "This will show the gameboard"
+    return board.to_text()
 
 @app.route("/start", methods=['POST'])
 def start():
@@ -18,10 +25,15 @@ def draw():
     """Draw a card into players hand"""
     pass
 
-@app.route("/play", methods=['POST'])
+@app.route("/play", methods=['GET'])
 def play():
     """Play a card and claim a space"""
-    pass
+    color = request.args.get('color')
+    num = int(request.args.get('num'))
+    x = int(request.args.get('x'))
+    y = int(request.args.get('y'))
+    board.play(x, y, color, num)
+    return board.to_text()
 
 @app.route("/reset", methods=['POST'])
 def reset():
